e11d86997b84245c944dff557e472aa0e60b4929
Towry Wang <towry@users.noreply.github.com>
2024-07-18 16:54:52 +0800

    feat: <🍋> add plugin carbon

---
+++ lua/plugins/utils/carbon.lua
@@ -0,0 +1,15 @@
+return {
+  "ellisonleao/carbon-now.nvim",
+  lazy = true,
+  vscode = true,
+  cmd = "CarbonNow",
+  opts = {
+    open_cmd = "open",
+    base_url = "https://carbon.now.sh/",
+    options = {
+      theme = "nord",
+      font_family = "JetBrains Mono",
+      font_size = "16px",
+    },
+  },
+}
