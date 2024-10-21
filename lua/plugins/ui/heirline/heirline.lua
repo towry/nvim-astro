08c6cea4bc978ed153b775bce811162224c22c49
Towry Wang <towry@users.noreply.github.com>
2024-10-21 13:13:33 +0800

    fix: duplicate config

--- lua/plugins/ui/heirline/heirline.lua
+++ lua/plugins/ui/heirline/heirline.lua
@@ -17,7 +17,7 @@ return {
         -- define the separators between each section
         separators = {
           left = { "", "" }, -- separator for the left side of the statusline
-          right = { "▐", "" }, -- separator for the right side of the statusline
+          right = { "", "" }, -- separator for the right side of the statusline
           tab = { "", "" },
         },
         -- add new colors that can be used by heirline
