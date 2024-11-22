2273a2a06a07b71992866c79c2f0baf01ad663ba
Towry Wang <towry@users.noreply.github.com>
2024-11-22 13:20:15 +0800

    chore: clean up

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -144,35 +144,14 @@ return {
           },
         },
       },
+
       {
-        "ckyrouac/darcula-solid.nvim",
-        dependencies = {
-          "rktjmp/lush.nvim",
-        },
-        lazy = (vim.g.colorscheme):match("dark") == nil,
-        config = function() end,
-      },
-      {
-        "miikanissi/modus-themes.nvim",
-        lazy = (vim.g.colorscheme):match("modus") == nil,
-        config = function()
-          require("modus-themes").setup({
-            style = "auto",
-            variant = "tinted", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
-            transparent = false,
-            dim_inactive = false,
-            hide_inactive_statusline = false,
-            styles = {
-              comments = { italic = true },
-              keywords = { italic = true },
-              functions = {},
-              variables = {},
-            },
-          })
-        end,
+        "nuvic/flexoki-nvim",
+        name = "flexoki",
+        priority = 1000,
+        event = "VeryLazy",
+        cond = (vim.g.colorscheme):match("flexoki") ~= nil,
       },
     },
   },
-  -- require("plugins.ui.catppucin"),
-  -- require("plugins.ui.gruvbox"),
 }
--- lua/plugins/ui/fidget.lua
+++ lua/plugins/ui/fidget.lua
@@ -16,9 +16,19 @@ return {
       },
     },
     notification = {
+      poll_rate = 100,
       override_vim_notify = true,
+      view = {
+        -- fix https://github.com/yioneko/vtsls/issues/159
+        render_message = function(msg, count) 
+          if msg and msg:match("vtsls:") then
+            return ""
+          end
+          return (cnt ~= nil and cnt <= 1) and msg or string.format("(%dx) %s", cnt or 0, msg or "")
+        end
+      },
       window = {
-        winblend = 15,
+        winblend = 35,
         normal_hl = "NormalFloat",
         max_width = 50,
         border = "solid",
