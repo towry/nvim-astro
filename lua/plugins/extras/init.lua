b709f2fe3df8bc0bc4f36ebf5b9eb1c0c4712636
Towry Wang <towry@users.noreply.github.com>
2024-12-13 17:10:05 +0800

    chore: disable copilot

--- lua/plugins/extras/init.lua
+++ lua/plugins/extras/init.lua
@@ -8,7 +8,7 @@ return {
   { import = "plugins.extras.trailblazer" },
   { import = "plugins.extras.codeium" },
   { import = "plugins.extras.codecompanion" },
-  { import = "plugins.extras.copilot" },
+  -- { import = "plugins.extras.copilot" },
   {
     "echasnovski/mini.move",
     keys = {
