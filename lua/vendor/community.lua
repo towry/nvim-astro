b77c3adaa04bbc43bb309cdc1372bbabdb595317
Towry Wang <towry@users.noreply.github.com>
2025-01-13 15:33:23 +0800

    chore: add rainbow delimiter plugin

--- lua/vendor/community.lua
+++ lua/vendor/community.lua
@@ -2,6 +2,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.lsp.nvim-lint" },
   { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   -- { import = "astrocommunity.completion.blink-cmp" },
   --- langs
   -- { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
