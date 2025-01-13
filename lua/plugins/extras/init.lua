31d396d71c7bcfdc010fdae836a7fbbe9f0ff0b8
Towry Wang <towry@users.noreply.github.com>
2025-01-13 10:48:33 +0800

    chore(ai): setup codecompanion for deepseek

--- lua/plugins/extras/init.lua
+++ lua/plugins/extras/init.lua
@@ -7,8 +7,8 @@ return {
   { import = "plugins.extras.window-picker" },
   { import = "plugins.extras.trailblazer" },
   { import = "plugins.extras.codeium" },
-  { import = "plugins.extras.avante" },
-  -- { import = "plugins.extras.codecompanion" },
+  -- { import = "plugins.extras.avante" },
+  { import = "plugins.extras.codecompanion" },
   -- { import = "plugins.extras.copilot" },
   {
     "echasnovski/mini.move",
