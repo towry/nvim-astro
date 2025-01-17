6b8ecd0592698bf327f27619f637350dd1a7c9cc
Towry Wang <towry@users.noreply.github.com>
2025-01-17 10:06:59 +0800

    chore: st

--- lua/plugins/core/astrocore.lua
+++ lua/plugins/core/astrocore.lua
@@ -55,6 +55,7 @@ return {
     options = {
       opt = { -- vim.opt.<key>
         laststatus = 3,
+        showtabline = 0,
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
