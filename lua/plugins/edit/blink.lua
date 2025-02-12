c44ff24b544e02b6bc11111141d75a909d4c1ed0
Towry Wang <towry@users.noreply.github.com>
2025-02-12 14:33:20 +0800

    fix: blink

--- lua/plugins/edit/blink.lua
+++ lua/plugins/edit/blink.lua
@@ -1,6 +1,8 @@
 return {
   "Saghen/blink.cmp",
+  version = false,
   optional = true,
+  build = "nix run .#build-plugin",
   opts = function(_, opts)
     opts.completion = vim.tbl_deep_extend("force", opts.completion or {}, {
       menu = vim.tbl_deep_extend("force", opts.completion and opts.completion.menu or {}, {
