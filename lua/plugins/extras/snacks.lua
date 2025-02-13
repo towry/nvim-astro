94be8e9f6132b715af220ba3f6455b31300da047
Towry Wang <towry@users.noreply.github.com>
2025-02-13 12:53:31 +0800

    chore: migrate to snacks

--- lua/plugins/extras/snacks.lua
+++ lua/plugins/extras/snacks.lua
@@ -1,14 +1,59 @@
+local V = require("v")
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   opts = {
-    picker = { ui_select = true },
+    picker = {
+      ui_select = true,
+      layout = {
+        cycle = true,
+        preset = function() return vim.o.columns >= 120 and "default" or "vertical" end,
+      },
+      layouts = {
+        vertical = {
+          layout = {
+            backdrop = false,
+            width = 0.5,
+            min_width = 80,
+            height = 0.8,
+            min_height = 30,
+            box = "vertical",
+            border = "single",
+            title = "{title} {live} {flags}",
+            title_pos = "center",
+            { win = "input", height = 1, border = "bottom" },
+            { win = "list", border = "none" },
+            { win = "preview", title = "{preview}", height = 0.4, border = "top" },
+          },
+        },
+        default = {
+          layout = {
+            box = "horizontal",
+            width = 0.8,
+            min_width = 120,
+            height = 0.8,
+            {
+              box = "vertical",
+              border = "single",
+              title = "{title} {live} {flags}",
+              { win = "input", height = 1, border = "bottom" },
+              { win = "list", border = "none" },
+            },
+            { win = "preview", title = "{preview}", border = "single", width = 0.5 },
+          },
+        },
+      },
+    },
     input = {},
     indent = {},
     scope = {},
     scratch = {},
+    explorer = {
+      replace_netrw = true,
+    },
   },
   specs = {
     {
@@ -26,6 +71,43 @@ return {
         maps.n["<Leader>xS"] = {
           function() Snacks.scratch.select() end,
           desc = "Select Scratch",
+        }
+
+        ----- picker
+        maps.n["<Leader>ff"] = {
+          function()
+            Snacks.picker.files({
+              cwd = V.nvim_root(),
+            })
+          end,
+          desc = "Find files",
+        }
+        maps.n["<Leader>fF"] = {
+          function() Snacks.picker.git_files() end,
+          desc = "Find git files",
+        }
+        maps.n["<leader>f<tab>"] = {
+          function()
+            Snacks.picker.recent({
+              cwd = V.nvim_workspaces_root(),
+            })
+          end,
+          desc = "Find recent",
+        }
+        -- buffers
+        maps.n["<localleader>,"] = {
+          function() Snacks.picker.buffers() end,
+          desc = "Buffers",
+        }
+        -- quickfix
+        maps.n["<Leader>fq"] = {
+          function() Snacks.picker.qflist() end,
+          desc = "Quickfix",
+        }
+        -- jumps
+        maps.n["<Leader>fj"] = {
+          function() Snacks.picker.jumps() end,
+          desc = "Jumps",
         }
       end,
     },
