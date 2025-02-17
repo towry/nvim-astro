f41f2a762b20af04764f33f6473280f7d5a06b7d
Towry Wang <towry@users.noreply.github.com>
2025-02-17 17:20:35 +0800

    chore: update

--- lua/plugins/edit/mappings.lua
+++ lua/plugins/edit/mappings.lua
@@ -48,14 +48,6 @@ return {
           -- second key is the lefthand side of the map
           -- mappings seen under group name "Buffer"
           ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-          ["<Leader>bD"] = {
-            function()
-              require("astroui.status").heirline.buffer_picker(
-                function(bufnr) require("astrocore.buffer").close(bufnr) end
-              )
-            end,
-            desc = "Pick to close",
-          },
           -- tables with the `name` key will be registered with which-key if it's installed
           -- this is useful for naming menus
           ["<Leader>b"] = { name = "Buffers" },
