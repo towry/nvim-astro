e859b7a0546a98160ed8232ada22159e056f518d
Towry Wang <towry@users.noreply.github.com>
2025-02-18 16:19:37 +0800

    disable plugin

--- lua/vendor/community.lua
+++ lua/vendor/community.lua
@@ -2,7 +2,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.lsp.nvim-lint" },
   { import = "astrocommunity.editing-support.conform-nvim" },
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  -- { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   -- { import = "astrocommunity.completion.blink-cmp" },
   --- langs
   { import = "astrocommunity.pack.rust" },
