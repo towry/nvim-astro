06bb014c5550b7dc02be3fa4c3d4ecbe82074936
Towry Wang <towry@users.noreply.github.com>
2025-02-18 11:55:10 +0800

    chore: add jj support

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -10,9 +10,13 @@ return {
     config = function() require("hunk").setup() end,
   },
   {
-    -- TODO: how to start inside nvim automatically
+    "rafikdraoui/jj-diffconflicts",
+    cmd = { "JJDiffConflicts" },
+  },
+  {
     "whiteinge/diffconflicts",
     cmd = { "DiffConflicts", "DiffConflictsWithHistory" },
+    cond = function() return not vim.g.jj_diffconflicts_marker_length end,
     event = "User AstroGitFile",
     lazy = not V.git_start_nvim(),
     config = function() end,
