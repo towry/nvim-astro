9d62aab18b78701e3a239f841b85e2ba92904515
Towry Wang <towry@users.noreply.github.com>
2025-02-20 12:55:35 +0800

    enable lsp signature help

--- lua/plugins/lsp/astrolsp.lua
+++ lua/plugins/lsp/astrolsp.lua
@@ -24,7 +24,7 @@ return {
       codelens = true, -- enable/disable codelens refresh on start
       inlay_hints = true, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
-      signature_help = false,
+      signature_help = true,
     },
     -- customize lsp formatting options
     formatting = {
