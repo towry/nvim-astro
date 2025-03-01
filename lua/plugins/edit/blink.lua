214b97baf1c74b9d5e291d2113b0a53ef169a77f
Towry Wang <towry@users.noreply.github.com>
2025-03-01 12:58:30 +0800

    copilot: change key for next/prev

--- lua/plugins/edit/blink.lua
+++ lua/plugins/edit/blink.lua
@@ -57,31 +57,17 @@ return {
       end,
       "fallback",
     }
-    opts.keymap["<C-P>"] = {
-      function(cmp)
-        if cmp.is_menu_visible() and cmp.get_selected_item() then return cmp.select_prev() end
-
-        if V.plugin_has_ai_suggestions() then
-          if cmp.is_menu_visible() then cmp.hide() end
-          vim.fn["copilot#Previous"]()
-          return
-        end
-
-        return cmp.show()
-      end,
-    }
-    opts.keymap["<C-N>"] = {
-      function(cmp)
-        if cmp.is_menu_visible() and cmp.get_selected_item() then return cmp.select_next() end
-
-        if V.plugin_has_ai_suggestions() then
-          if cmp.is_menu_visible() then cmp.hide() end
-          vim.fn["copilot#Next"]()
-          return
-        end
-
-        return cmp.show()
-      end,
-    }
+    -- opts.keymap["<C-P>"] = {
+    --   function(cmp)
+    --     if cmp.is_menu_visible() then return cmp.select_prev() end
+    --     return cmp.show()
+    --   end,
+    -- }
+    -- opts.keymap["<C-N>"] = {
+    --   function(cmp)
+    --     if cmp.is_menu_visible() then return cmp.select_next() end
+    --     return cmp.show()
+    --   end,
+    -- }
   end,
 }
