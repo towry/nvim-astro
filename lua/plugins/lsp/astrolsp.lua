0e8e487fa560f6a1843ac47ede4b237162501343
Towry Wang <towry@users.noreply.github.com>
2025-03-05 10:16:34 +0800

    disable emmet_ls

--- lua/plugins/lsp/astrolsp.lua
+++ lua/plugins/lsp/astrolsp.lua
@@ -182,6 +182,7 @@ return {
         ts_ls = false,
         volar = false,
         eslint = false,
+        emmet_ls = false,
         -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
         -- function(server, opts) require("lspconfig")[server].setup(opts) end
 
