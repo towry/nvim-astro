b384757374b9fd2f1cb888ca07c9e2680f674a33
Towry Wang <8279858+towry@users.noreply.github.com>
2025-03-17 17:57:17 +0800

    next (#29)
    * fix nvim-lint
    
    * update dep
    
    * update
    
    * disable mini.diff mappings
    
    * update
    
    * update dep
    
    ---------
    
    Co-authored-by: Towry Wang <towry@users.noreply.github.com>

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -2,6 +2,7 @@ if vim.g.vscode then return {} end
 
 return {
   { import = "plugins.lsp.astrolsp" },
+  { import = "plugins.lsp.mason" },
   { import = "plugins.lsp.conform" },
   { import = "plugins.lsp.nvim-lint" },
 }
