f3bf6bb6a7a4b41cb7377bc42516b3fa756a4d3f
Towry Wang <towry@users.noreply.github.com>
2025-05-23 17:21:16 +0800

    change theme

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,4 +1,4 @@
-vim.g.colorscheme = "jb"
+vim.g.colorscheme = "kanagawa"
 
 if vim.g.vscode then return {} end
 
--- lua/pre_setup.lua
+++ lua/pre_setup.lua
@@ -1,14 +1,14 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
-require("vim._extui").enable({
-  enable = true,
-  msg = {
-    pos = "box",
-    box = {
-      timeout = 4000,
-    },
-  },
-})
+-- require("vim._extui").enable({
+--   enable = false,
+--   msg = {
+--     pos = "box",
+--     box = {
+--       timeout = 4000,
+--     },
+--   },
+-- })
 
 require("vendor.options")
